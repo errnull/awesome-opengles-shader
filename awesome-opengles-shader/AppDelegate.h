64df6b99bfc90f4e50444fb35142b6bfbd599048
errnull <errnull@libsvg.com>
2023-04-24 15:31:48 +0800

    Initial Commit

---
+++ awesome-opengles-shader/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  awesome-opengles-shader
+//
+//  Created by errnull on 2023/4/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ awesome-opengles-shader/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  awesome-opengles-shader
+//
+//  Created by errnull on 2023/4/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
